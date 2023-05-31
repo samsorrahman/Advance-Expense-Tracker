ed39e8fdde60ef7de1f28dba1ff7a3a9c1537040
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-31 11:05:32 +0500

    add functionality added

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
+from .forms import ExpenseForm
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'myapp/index.html')
+    expense_form = ExpenseForm()
+
+    if request.method == "POST":
+        expense = ExpenseForm(request.POST)
+        if expense.is_valid():
+            expense.save()
+
+    context = {
+        'expense_form': expense_form,
+    }
+    return render(request, 'myapp/index.html', context)

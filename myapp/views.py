65fa0d0698601c31322b70580cb97f05d0320f62
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-05-31 16:03:04 +0500

    lastweek, lastmonth, lastyear sum calculation

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import ExpenseForm
 from .models import Expense
+import datetime
 from django.db.models import Sum
 # Create your views here.
 
@@ -15,10 +16,26 @@ def index(request):
 
     expenses = Expense.objects.all()
     total_expenses = expenses.aggregate(Sum('amount'))
+
+    last_year = datetime.date.today()-datetime.timedelta(days=365)
+    data = Expense.objects.filter(date__gt=last_year)
+    yearly_sum = data.aggregate(Sum('amount'))
+
+    last_month = datetime.date.today()-datetime.timedelta(days=30)
+    data = Expense.objects.filter(date__gt=last_month)
+    monthly_sum = data.aggregate(Sum('amount'))
+
+    last_week = datetime.date.today()-datetime.timedelta(days=7)
+    data = Expense.objects.filter(date__gt=last_week)
+    weekly_sum = data.aggregate(Sum('amount'))
+
     context = {
         'expense_form': expense_form,
         'expenses': expenses,
-        'total_expenses': total_expenses
+        'total_expenses': total_expenses,
+        'yearly_sum': yearly_sum,
+        'monthly_sum': monthly_sum,
+        'weekly_sum': weekly_sum,
     }
     return render(request, 'myapp/index.html', context)
 

f2121feaaa3735ec689d4c94d128ea9f1b9543e1
Samsor Rahman <112087807+samsorrahman@users.noreply.github.com>
2023-06-04 18:08:44 +0500

    Registration and login functionality added

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('edit/<int:id>/', views.edit, name='edit'),
     path('delete/<int:id>/', views.delete, name='delete'),
-
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerUser, name="register"),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -3,9 +3,15 @@ from .forms import ExpenseForm
 from .models import Expense
 import datetime
 from django.db.models import Sum
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+
 # Create your views here.
 
-
+# @login_required(login_url='login')
 def index(request):
     expense_form = ExpenseForm()
 
@@ -48,6 +54,7 @@ def index(request):
     return render(request, 'myapp/index.html', context)
 
 
+# @login_required(login_url='login')
 def edit(request, id):
     expense = Expense.objects.get(id=id)
     expense_form = ExpenseForm(instance=expense)
@@ -66,9 +73,75 @@ def edit(request, id):
     return render(request, 'myapp/edit.html', context)
 
 
+# @login_required(login_url='login')
 def delete(request, id):
     if request.method == 'POST' and 'delete' in request.POST:
         expense = Expense.objects.get(id=id)
         expense.delete()
 
     return redirect('index')
+
+
+def logoutUser(request):
+    logout(request)
+    messages.info(request, 'User was logged out!')
+    return redirect('login')
+
+
+def registerUser(request):
+    if request.method == 'POST':
+        # Retrieve the form data
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        # Check if passwords match
+        if password != confirm_password:
+            # Passwords do not match, handle the error (e.g., show an error message)
+            # You can redirect to the registration page or display an error message
+            messages.error(request, 'Password does not match!')
+            return redirect('register')
+
+        # Create the user
+        user = User.objects.create_user(
+            username=username, password=password, email=email, first_name=name)
+
+        # You can also set additional fields if needed, such as user profile or extra information
+
+        # Save the user to the database
+        user.save()
+
+        # Redirect to a success page or login page
+        return redirect('index')
+
+    # Render the registration template if the request method is not POST
+    page = 'register'
+    context = {
+        'page': page
+    }
+    return render(request, 'myapp/registration.html', context)
+
+
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('index')
+
+    if request.method == 'POST':
+        username = request.POST['username'].lower()
+        password = request.POST['password']
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'Username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            messages.error(request, 'Username Or password is incorrect')
+    return render(request, 'myapp/registration.html')
